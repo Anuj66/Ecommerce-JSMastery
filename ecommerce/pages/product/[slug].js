b18d09ce0f7e84bb3720ae9cdf7f21d066865fa7
Anuj Saraogi <47737640+Anuj66@users.noreply.github.com>
2022-06-30 00:56:06 +0530

    Added slug

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -1,5 +1,11 @@
 import React from "react";
 import { client, urlFor } from "../../lib/client";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
@@ -10,6 +16,41 @@ const ProductDetails = ({ product, products }) => {
         <div>
           <div className="image-container">
             <img src={urlFor(image && image[0])} />
+          </div>
+          {/* <div className="small-images-container">
+            {image?.map((item, i) => {
+              <img src={urlFor(item)} className="" onMouseEnter={""} />;
+            })}
+          </div> */}
+        </div>
+        <div className="product-details-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onClick="">
+                0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
           </div>
         </div>
       </div>
@@ -24,6 +65,24 @@ export const getStaticProps = async ({ params: { slug } }) => {
   const products = await client.fetch(productsQuery);
   return {
     props: { product, products },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[__type == "product"] {
+    slug {
+      current
+    }
+  }`;
+
+  const products = await client.fetch(query);
+  const paths = products.map((product) => ({
+    params: { slug: product.slug.current },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
   };
 };
 
